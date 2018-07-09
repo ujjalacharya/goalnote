cd98fcc0b5706ee5df5595c12777e1e23dfbdad3
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-10 01:53:55 +0545

    adds add route

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 3000;
 
+
+
+//Connect to the database
+mongoose.connect('mongodb://localhost/vidjoid-dev')
+        .then(()=>{
+            console.log('Successfully connected to the database')
+        })
+        .catch(err=>console.log(err))
+
+//Import Model
+require('./models/Ideas');
+const Idea = mongoose.model('ideas');
+//Template engine middleware
 const exphbs = require('express-handlebars');
 
 app.engine('handlebars',
@@ -10,12 +24,17 @@ app.engine('handlebars',
 
 app.set('view engine', 'handlebars');
 
+//Route handling
 app.get('/', (req, res) => {
     res.render('index');
 })
 
 app.get('/about', (req, res) => {
     res.render('about');
+})
+
+app.get('/ideas/add', (req, res)=>{
+    res.render('ideas/add');
 })
 
 
