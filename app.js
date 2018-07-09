f058499681a5eaa225ce18818ee3563173cb2706
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-09 17:58:08 +0545

    adds bootstrap

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const exphbs = require('express-handlebars');
 
-app.get('/', (req, res)=>{
-    res.send('Suo');
+app.engine('handlebars',
+    exphbs({ defaultLayout: 'main' })
+);
+
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
-app.listen(PORT, ()=>{
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+
+app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`)
 })
