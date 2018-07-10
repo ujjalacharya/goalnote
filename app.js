6fd38ca7e98db82598cf06d7ee610c83a7ec7c88
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-10 08:13:35 +0545

    adds server side validation

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 3000;
-
+const bodyParser = require('body-parser')
 
 
 //Connect to the database
 mongoose.connect('mongodb://localhost/vidjoid-dev')
-        .then(()=>{
-            console.log('Successfully connected to the database')
-        })
-        .catch(err=>console.log(err))
+    .then(() => {
+        console.log('Successfully connected to the database')
+    })
+    .catch(err => console.log(err))
 
 //Import Model
 require('./models/Ideas');
 const Idea = mongoose.model('ideas');
+
+//BodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //Template engine middleware
 const exphbs = require('express-handlebars');
 
@@ -23,6 +28,7 @@ app.engine('handlebars',
 );
 
 app.set('view engine', 'handlebars');
+
 
 //Route handling
 app.get('/', (req, res) => {
@@ -33,8 +39,27 @@ app.get('/about', (req, res) => {
     res.render('about');
 })
 
-app.get('/ideas/add', (req, res)=>{
+app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+})
+
+app.post('/ideas', (req, res) => {
+    const errors = [];
+    if (!req.body.title) {
+        errors.push({ text: 'Please enter the title' })
+    }
+    if (!req.body.desc) {
+        errors.push({ text: 'Please enter the description' })
+    }
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors,
+            title: req.body.title,
+            desc: req.body.desc
+        })
+    } else {
+        res.send('OK')
+    }
 })
 
 
