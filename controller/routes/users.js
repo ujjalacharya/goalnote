d8aba2bcf2da834c5b836798605e4fd02f93bcbe
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-11 16:44:30 +0545

    makes user model and adds registration validated page

--- controller/routes/users.js
+++ controller/routes/users.js
@@ -2,12 +2,42 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const Idea = require('../../models/Idea');
 
-router.get('/login', (req, res)=>{
+router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
     res.render('users/register')
+})
+
+router.post('/register', (req, res) => {
+    const errors = [];
+    if (!req.body.name) {
+        errors.push({ text: 'Empty name' })
+    }
+    if (!req.body.email) {
+        errors.push({ text: 'Empty email' })
+    }
+    if (req.body.password.length < 4) {
+        errors.push({ text: 'Password too short' })
+    }
+    if (req.body.password !== req.body.password2) {
+        errors.push({ text: 'Passwords do not match' })
+    }
+    
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            password: req.body.password,
+            password2: req.body.password2,
+            email: req.body.email
+        })
+    }
+    else {
+        res.send('Successfully registered!')
+    }
+
 })
 
 module.exports = router;
