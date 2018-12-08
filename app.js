6bd582317d4c559779167a8be6fc9855fa5fc2f2
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-12-08 13:36:31 +0545

    add pagination feature

--- app.js
+++ app.js
@@ -1,85 +1,90 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
+const mongoose = require("mongoose");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const session = require('express-session')
-const flash = require('connect-flash');
-const ideas = require('./controller/routes/ideas');
-const users = require('./controller/routes/users');
-const passport = require('passport');
-const db = require('./config/db')
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ideas = require("./controller/routes/ideas");
+const users = require("./controller/routes/users");
+const passport = require("passport");
+const db = require("./config/db");
+
+const { paginate } = require("./helpers/handlebars-helpers");
 
 //Maintainence
 // app.use(function(req, res, next){
 //     res.send('<h2>App is in the maintainence mode right now. Please visit us later</h2>')
 // })
 
-
 mongoose.Promise = global.Promise;
 //Connect to the database
-mongoose.connect(db.mongoURI)
-    .then(() => {
-        console.log('Successfully connected to the database')
-    })
-    .catch(err => console.log(err))
+mongoose
+  .connect(db.mongoURI, {useNewUrlParser: true})
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => console.log(err));
 
 //Middleware for static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //BodyParser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Passport JS
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Template engine middleware
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-app.engine('handlebars',
-    exphbs({ defaultLayout: 'main' })
+app.engine(
+  "handlebars",
+  exphbs({ defaultLayout: "main", helpers: { paginate }})
 );
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 //Middleware for method-override
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 //Middlware for session and flash
-app.use(session({
-    secret: 'ujjals secret',
+app.use(
+  session({
+    secret: "ujjals secret",
     resave: false,
-    saveUninitialized: true,
-}))
+    saveUninitialized: true
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null
-    next();
-})
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
 
 //Route handling
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.use('/ideas', ideas);
-app.use('/users', users);
+app.use("/ideas", ideas);
+app.use("/users", users);
 
 app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
-})
+  console.log(`Server started at port ${PORT}`);
+});
--- controller/routes/ideas.js
+++ controller/routes/ideas.js
@@ -1,99 +1,130 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const Idea = require('../../models/Idea');
-const { ensureAuthentication } = require('../../helpers/auth');
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Idea = require("../../models/Idea");
+const { ensureAuthentication } = require("../../helpers/auth");
 
-router.get('/add', ensureAuthentication, (req, res) => {
-    res.render('ideas/add');
-})
+router.get("/add", ensureAuthentication, (req, res) => {
+  res.render("ideas/add");
+});
 
-router.post('/', ensureAuthentication, (req, res) => {
-    const errors = [];
-    if (!req.body.title) {
-        errors.push({ text: 'Please enter the title' })
-    }
-    if (!req.body.details) {
-        errors.push({ text: 'Please enter the description' })
-    }
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors,
-            title: req.body.title,
-            details: req.body.details
-        })
+router.post("/", ensureAuthentication, (req, res) => {
+  const errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please enter the title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please enter the description" });
+  }
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newDetails = {
+      title: req.body.title,
+      details: req.body.details,
+      user: req.user.id
+    };
+    new Idea(newDetails).save().then(data => {
+      req.flash("success_msg", "Goal idea has been added");
+      res.redirect("/ideas");
+    });
+  }
+});
+
+router.get("/", ensureAuthentication, (req, res) => {
+  Idea.countDocuments({isCompleted: false}).then(ideasCount => {
+    if (ideasCount < 1) {
+      res.render("ideas", {
+        Nogoals: "No goals listed!"
+      });
     } else {
-        const newDetails = {
-            title: req.body.title,
-            details: req.body.details,
-            user: req.user.id
-        }
-        new Idea(newDetails)
-            .save()
-            .then(data => {
-                req.flash('success_msg', 'Video idea has been added')
-                res.redirect('/ideas')
-            })
+      const perPage = 5;
+      const page = req.query.page || 1;
+      Idea.find({ user: req.user.id, isCompleted: false })
+        .sort({ date: "desc" })
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .then(data => {
+          res.render("ideas", {
+            data,
+            current: parseInt(page),
+            pages: Math.ceil(ideasCount / perPage),
+            showPagination: req.query.page ? true : data.length +1 > perPage //send Boolean value to check if pagination should be shown
+          });
+        })
+        .catch(err => console.log(err));
     }
-})
+  });
+});
 
-router.get('/', ensureAuthentication, (req, res) => {
-    Idea.find({ user: req.user.id, isCompleted: false })
-        .sort({ date: 'desc' })
-        .then((data) => {
-            res.render('ideas', {
-                data
-            })
-        })
-})
-router.get('/completed', ensureAuthentication, (req, res) => {
-    Idea.find({ user: req.user.id, isCompleted: true })
-        .sort({ date: 'desc' })
-        .then((data) => {
-            res.render('ideas', {
-                data
-            })
-        })
-})
-
-router.get('/edit/:id', ensureAuthentication, (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then((data) => {
-            if (data.user !== req.user.id) {
-                req.flash('error_msg', 'Not Authorized');
-                res.redirect('/ideas')
-            } else {
-                res.render('ideas/edit', { data, isCompleted: data.isCompleted ? "checked": null })
-            }
-        })
-})
-
-router.put('/:id', ensureAuthentication, (req, res) => {
-    console.log(req.body)
-    Idea.findOne({
-        _id: req.params.id,
-    })
+router.get("/completed", ensureAuthentication, (req, res) => {
+  Idea.countDocuments({isCompleted: true}).then(ideasCount => {
+    if (ideasCount < 1) {
+      res.render("ideas", {
+        Nogoals: "No goals listed!"
+      });
+    } else {
+      const perPage = 5;
+      const page = req.query.page || 1;
+      Idea.find({ user: req.user.id, isCompleted: true })
+        .sort({ date: "desc" })
+        .skip(perPage * page - perPage)
+        .limit(perPage)
         .then(data => {
-            data.title = req.body.title,
-            data.details = req.body.details
-            data.isCompleted = req.body.isCompleted ? true : false
-            data.save()
-        })
-        .then(data => {
-            req.flash('success_msg', 'Video idea has been updated')
-            res.redirect('/ideas')
-        })
-})
+            res.render("ideas", {
+              data,
+              current: parseInt(page),
+              pages: Math.ceil(ideasCount / perPage),
+              showPagination: req.query.page ? true : data.length +1 > perPage //send Boolean value to check if pagination should be shown
+            });
+          })
+        .catch(err => console.log(err));
+    }
+  });
+});
 
-router.delete('/:id', ensureAuthentication, (req, res) => {
-    Idea.findOneAndRemove({
-        _id: req.params.id
+router.get("/edit/:id", ensureAuthentication, (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(data => {
+    if (data.user !== req.user.id) {
+      req.flash("error_msg", "Not Authorized");
+      res.redirect("/ideas");
+    } else {
+      res.render("ideas/edit", {
+        data,
+        isCompleted: data.isCompleted ? "checked" : null
+      });
+    }
+  });
+});
+
+router.put("/:id", ensureAuthentication, (req, res) => {
+  console.log(req.body);
+  Idea.findOne({
+    _id: req.params.id
+  })
+    .then(data => {
+      (data.title = req.body.title), (data.details = req.body.details);
+      data.isCompleted = req.body.isCompleted ? true : false;
+      data.save();
     })
-        .then(data => {
-            req.flash('success_msg', 'Video idea has been deleted')
-            res.redirect('/ideas')
-        })
-})
+    .then(data => {
+      req.flash("success_msg", "Video idea has been updated");
+      res.redirect("/ideas");
+    });
+});
+
+router.delete("/:id", ensureAuthentication, (req, res) => {
+  Idea.findOneAndRemove({
+    _id: req.params.id
+  }).then(data => {
+    req.flash("success_msg", "Video idea has been deleted");
+    res.redirect("/ideas");
+  });
+});
 
 module.exports = router;
